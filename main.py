73c871e5768d54cf1910b152e1bb99953fe915c7
Robodlan <azuldevs@zohomail.com>
2024-07-19 12:29:55 -0400

    modified2 main

--- main.py
+++ main.py
@@ -8,3 +8,19 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+
+let = "".join(random.sample(letters, nr_letters))
+sym = "".join(random.sample(symbols, nr_symbols))
+num = "".join(random.sample(numbers, nr_numbers))
+
+password = ""
+for x in let:
+    password += x
+for y in sym:
+    password += y
+for z in num:
+    password += z
+l = list(password)
+random.shuffle(l)
+result = "".join(l)
+print(result)
